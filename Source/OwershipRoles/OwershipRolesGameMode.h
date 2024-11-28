79d58ee9448a5ae73f0b53d4ea8714d486c3488e
like-coder <81460494+like-coder@users.noreply.github.com>
2024-11-28 23:10:14 +0800

    123456

---
+++ Source/OwershipRoles/OwershipRolesGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OwershipRolesGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AOwershipRolesGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AOwershipRolesGameMode();
+};
+
+
+
